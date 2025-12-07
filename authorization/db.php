4d259a35ec124cb7e049602da7c752f16ddb2350
Resha <reshalii3dit14@gmail.com>
2025-12-07 04:05:15 +0300

    Добавил докер и докер compose. Контейнеры запускаются, но проблема с авторизацией. Проблема в синхронизации пароля докера с SQL базой

--- authorization/db.php
+++ authorization/db.php
@@ -1,20 +1,17 @@
 <?php
-// Стартуем сессию, если она ещё не была запущена
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-// Данные для подключения к слонуSQL
-$host = 'localhost';
+$host = 'db';
 $port = '5432';
 $dbname = 'auth_db';
 $user = 'postgres';
 $pass = 'Resh1900$$#KKoPP';
 
-// Создание подключения к слонуSQL
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass");
 
-if (!$conn) {
-    die("Ошибка подключения к базе данных: " . pg_last_error());
+if ($conn === false) {
+    die("Ошибка подключения к базе данных.");
 }
 ?>
